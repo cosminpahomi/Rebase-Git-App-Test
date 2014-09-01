732b5b751756b0339e81e34f31ec4e41c721303a
Cosmin Pahomi <cosmin.0303@gmail.com>
2014-09-01 14:22:33 +0300

    a

--- Another Folder/SCChildrenDashboardViewController.h
+++ Another Folder/SCChildrenDashboardViewController.h
@@ -5,11 +5,11 @@
 //  Created by LJ Kyser on 4/3/14.
 //  Copyright (c) 2014 Smart. All rights reserved.
 //
-
+Some changes here
 #import <UIKit/UIKit.h>
 #import "SCChildrenTableView.h"
 #import "SCStandardDashboardViewController.h"
 
 @interface SCChildrenDashboardViewController: SCStandardDashboardViewController <SCchildrenTableViewDelegate>
 
-@end
+@end and here
